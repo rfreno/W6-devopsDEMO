8c124b8ea255ba6a8b8481ec3d908f2d1a758838
rfreno <wintersbek@gmail.com>
2022-09-22 12:30:08 -0600

    ready for heroku

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'ddc85ba6e0e14531a6d0be2691a9ef3e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           rollbar.log('student was added successfully')
+           res.status(200).send(students)
+       } else if (name === ''){
+            rollbar.error('submission was empty')
+           res.status(400).send('You must enter a name.')
+       } else {
+            rollbar.error('student name already exists')
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    rollbar.info('Student was deleted')
+    res.status(200).send(students)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
